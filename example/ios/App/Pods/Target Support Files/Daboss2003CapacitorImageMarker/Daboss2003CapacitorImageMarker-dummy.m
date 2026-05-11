2dffb700a0b8d4e2fcb826cd4fba10cdde7b5e4e
Daboss <samsonoluwafemi203@gmail.com>
2026-05-11 16:08:36 +0100

    added the missing asset to example/android

---
+++ example/ios/App/Pods/Target Support Files/Daboss2003CapacitorImageMarker/Daboss2003CapacitorImageMarker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Daboss2003CapacitorImageMarker : NSObject
+@end
+@implementation PodsDummy_Daboss2003CapacitorImageMarker
+@end
---
+++ example/ios/App/Pods/Target Support Files/Daboss2003CapacitorImageMarker/Daboss2003CapacitorImageMarker-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "ImageMarkerPlugin.h"
+
+FOUNDATION_EXPORT double Daboss2003CapacitorImageMarkerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Daboss2003CapacitorImageMarkerVersionString[];
+
